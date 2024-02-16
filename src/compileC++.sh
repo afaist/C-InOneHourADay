8aaab61aef91cb5079daffe12822dfe512f224cd
afaist <afaist@gmail.com>
2024-02-16 17:43:49 +0700

    Добавлена проверка существования директории для исполняемых файлов

--- src/compileC++.sh
+++ src/compileC++.sh
@@ -1,5 +1,10 @@
 file_in=$1
 file_out="${file_in##*/}"
+if ! [ -d ../exe ]; then
+  printf "directory ../exe/ not found\\n"
+  printf "Create directory ../exe/\\n"
+  mkdir ../exe
+fi
 printf "compile file ${file_out} to file ../exe/${file_out%%.*}.out\\n"
 
 g++  $file_in -o ../exe/"${file_out%%.*}".out
