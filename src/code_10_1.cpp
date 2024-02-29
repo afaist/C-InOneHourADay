0dd24e9ce6b3934a71f71c554e9a733f8cad8d91
afaist <afaist@gmail.com>
2024-02-29 17:47:31 +0700

    Публичное наследование публичных членов класса

---
+++ src/code_10_1.cpp
@@ -0,0 +1,44 @@
+#include "terminal.h"
+#include <iostream>
+using namespace std;
+
+class Fish {
+public:
+  bool isFreshWaterFish;
+
+  void Swim() {
+    if (isFreshWaterFish) {
+      cout << "Он пресноводный" << endl;
+    } else
+      cout << "Он морской" << endl;
+  }
+};
+
+class Tuna : public Fish {
+public:
+  Tuna() {
+    isFreshWaterFish = false;
+    cout << "Это тунец!" << endl;
+  }
+};
+
+class Carp : public Fish {
+public:
+  Carp() {
+    isFreshWaterFish = true;
+    cout << "Это Карп!" << endl;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  cout << green_bold;
+  cout << "Пример публичного наследования рыб от класса Fish" << reset << endl;
+  cout << "Моя еда:" << endl;
+  cout << "Обед:" << endl;
+  Carp myLunch;
+  myLunch.Swim();
+  cout << "Ужин:" << endl;
+  Tuna myDinner;
+  myDinner.Swim();
+  return 0;
+}
