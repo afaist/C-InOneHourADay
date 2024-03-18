d79e0bad3e2984647bd4e1df6888fb8923490864
afaist <afaist@gmail.com>
2024-03-18 17:51:58 +0700

    Цвет в терминале. Добавлены строки для Unicode

--- src/terminal.h
+++ src/terminal.h
@@ -30,3 +30,32 @@ const std::string blue_italic("\033[3;34m");
 const std::string cyan_italic("\033[3;36m");
 const std::string magenta_italic("\033[3;35m");
 const std::string white_italic("\033[3;37m");
+
+
+const std::wstring w_reset(L"\033[0m");
+const std::wstring w_black(L"\033[0;30m");
+const std::wstring w_red(L"\033[0;31m");
+const std::wstring w_green(L"\033[0;32m");
+const std::wstring w_yellow(L"\033[0;33m");
+const std::wstring w_blue(L"\033[0;34m");
+const std::wstring w_cyan(L"\033[0;36m");
+const std::wstring w_magenta(L"\033[0;35m");
+const std::wstring w_white(L"\033[0;37m");
+
+const std::wstring w_black_bold(L"\033[1;30m");
+const std::wstring w_red_bold(L"\033[1;31m");
+const std::wstring w_green_bold(L"\033[1;32m");
+const std::wstring w_yellow_bold(L"\033[1;33m");
+const std::wstring w_blue_bold(L"\033[1;34m");
+const std::wstring w_cyan_bold(L"\033[1;36m");
+const std::wstring w_magenta_bold(L"\033[1;35m");
+const std::wstring w_white_bold(L"\033[1;37m");
+
+const std::wstring w_black_italic(L"\033[3;30m");
+const std::wstring w_red_italic(L"\033[3;31m");
+const std::wstring w_green_italic(L"\033[3;32m");
+const std::wstring w_yellow_italic(L"\033[3;33m");
+const std::wstring w_blue_italic(L"\033[3;34m");
+const std::wstring w_cyan_italic(L"\033[3;36m");
+const std::wstring w_magenta_italic(L"\033[3;35m");
+const std::wstring w_white_italic(L"\033[3;37m");
