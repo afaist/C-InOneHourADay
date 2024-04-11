b84d668e82a8ec4eabbe89fd5bebbac49030c47c
afaist <afaist@gmail.com>
2024-04-11 18:03:36 +0700

    Удаление из мультимножества с помощью функции-члена erase()

---
+++ src/code_19_4.cpp
@@ -0,0 +1,23 @@
+#include "terminal.h"
+#include <set>
+using namespace std;
+
+typedef multiset<int> MSETINT;
+
+int main(int argc, char *argv[]) {
+  cout << green_bold;
+  cout << "Использование функции-члена erase() мультимножества" << reset
+       << endl;
+
+  cout << "Создаем мультимножество" << endl;
+  MSETINT msetInts{43, 78, 78, -1, 124, -1, 22, 43, 5};
+  DisplayContainer(msetInts);
+  cout << "Введите удаляемое число: ";
+  int input = 0;
+  cin >> input;
+  cout << "Удаляем " << msetInts.count(input);
+  cout << " экземпляра значения " << input << endl;
+  msetInts.erase(input);
+  DisplayContainer(msetInts);
+  return 0;
+}
