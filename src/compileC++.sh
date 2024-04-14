64dd0f0fb4f7b8f006335735c97a21486373234a
afaist <afaist@gmail.com>
2024-04-14 20:16:35 +0700

    Добавлена возможность компиляции из папок

--- src/compileC++.sh
+++ src/compileC++.sh
@@ -1,10 +1,13 @@
 file_in=$1
+printf "file in $file_in\\n"
 file_out="${file_in##*/}"
+file_out=$(basename -- "$file_out")
+printf "file out $file_out\\n"
 if ! [ -d ../exe ]; then
   printf "directory ../exe/ not found\\n"
   printf "Create directory ../exe/\\n"
   mkdir ../exe
 fi
-printf "compile file ${file_out} to file ../exe/${file_out%%.*}.out\\n"
+printf "compile file ${file_in} to file ../exe/${file_out%%.*}.out\\n"
 
-g++  $file_in -o ../exe/"${file_out%%.*}".out
+g++  "$file_in" -o ../exe/"${file_out%%.*}".out
